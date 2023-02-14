7a9720a635a6b00de87aa62530f218d0a0ed3fff
Angga Saputra <masangga.com@gmail.com>
2023-02-15 00:27:57 +0700

    up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+const me = {
+  name: "Angga Saputra",
+  title: "Angga Saputra",
+  username: "@kangangga",
+  description:
+    "Saya adalah seorang software engineer dengan pengalaman lebih dari 7 tahun bekerja pada inisiatif pengembangan perangkat lunak yang kompleks. Saya bersemangat tentang pengembangan perangkat lunak dan saya menantikan untuk mengerjakan proyek baru dalam waktu dekat.",
+  keywords:
+    "angga saputra, angga, saputra, software, engineer, developer, fullstack, full-stack, full stack, web, mobile, mobile app, mobile application, mobile developer, mobile engineer, mobile software engineer, mobile software developer, mobile software developer engineer, mobile software developer engineer, mobile software developer engineer, mobile softwa",
+};
+
 export default defineNuxtConfig({
   ssr: false,
   css: [
@@ -17,11 +26,27 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      title: me.name,
       charset: "utf-16",
       viewport: "width=device-width, initial-scale=1.0",
       meta: [
-        // <meta name="description" content="My amazing site">
-        // { name: "description", content: "My amazing site." },
+        { name: "description", content: me.description },
+        {
+          name: "keywords",
+          content: me.keywords,
+        },
+        { property: "og:url", content: "https://kangangga.github.io" },
+        { property: "og:type", content: "website" },
+        { property: "og:title", content: me.title },
+        {
+          property: "og:image",
+          content: "https://kangangga.github.io/img/angga.png",
+        },
+        { property: "og:description", content: me.description },
+        { name: "twitter:card", content: "summary" },
+        { name: "twitter:site", content: me.username },
+        { name: "twitter:creator", content: me.username },
+        { name: "twitter:description", content: me.description },
       ],
       link: [
         { rel: "icon", type: "image/x-icon", href: "/img/favicon.ico" },
