c1203ce7b190c3163b16cf5e8aa4e3128858be02
kangangga <kangangga@users.noreply.github.com>
2023-02-14 21:51:37 +0000

    deploy: 5fd5731bb3b8d7dfddd279fb66a309f1c5f95eda

---
+++ _nuxt/app.config.97e0f3d3.js
@@ -0,0 +1,1 @@
+import{j as u,C as s}from"./entry.65a620be.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
--- _nuxt/app.config.980d9cc1.js
+++
@@ -1,1 +0,0 @@
-import{j as u,C as s}from"./entry.c1890e20.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
