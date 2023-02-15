4a3e263a956ec2b733dd29a3176a0f49336191dc
kangangga <kangangga@users.noreply.github.com>
2023-02-15 00:05:52 +0000

    deploy: 8a4bcfdae901e11b165a003023e7581baf491f95

--- _nuxt/app.config.c80116dd.js
+++
@@ -1,1 +0,0 @@
-import{i as u,j as a,k as n}from"./entry.ce11a30a.js";function r(e,s){return a()._useHead(e,s)}const i=e=>u(e),t={};n(t);export{r as a,i as u};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const options = {"autoRegister":true,"cacheOptions":{"directoryIndex":"/","revision":"9BcMSAjubLZ9"},"enabled":true,"preCaching":[],"templatePath":null,"workboxVersion":"6.5.3","workboxUrl":"https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js"}
+const options = {"autoRegister":true,"cacheOptions":{"directoryIndex":"/","revision":"2FZzUbDRDdPy"},"enabled":true,"preCaching":[],"templatePath":null,"workboxVersion":"6.5.3","workboxUrl":"https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js"}
 
 importScripts(options.workboxUrl)
 
